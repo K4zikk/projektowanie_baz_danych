c21571545204d2c7487905d7fce3c25f58d4dbfe
K4zikk <kacperbartkowiakjd@gmail.com>
2022-09-15 14:58:40 +0200

    sqlzapytania

---
+++ sqlzapyta.sql
@@ -0,0 +1,8 @@
+SELECT * FROM `champions` WHERE 1;
+SELECT * FROM `champions_has_runes` WHERE 1;
+SELECT * FROM `items` WHERE 1;
+SELECT * FROM `items_has_champions` WHERE 1;
+SELECT * FROM `maps` WHERE 1;
+SELECT * FROM `players` WHERE 1;
+SELECT * FROM `runes` WHERE 1;
+SELECT * FROM `shop` WHERE 1;
